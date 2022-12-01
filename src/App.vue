d26ad0de006efa7be9a7b7af6694c94999f116fd
Ciaowilly3 <muscomassi@gmail.com>
2022-12-01 15:16:27 +0100

    JumboTron

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
 import Titolo from "./components/AppTitle.vue";
+import JumboTron from "./components/JumboTron.vue";
 export default {
-  components: { Titolo },
+  components: { Titolo, JumboTron },
 };
 </script>
 
-<template><Titolo></Titolo></template>
+<template><Titolo></Titolo><JumboTron></JumboTron></template>
 
 <style scoped></style>
---
+++ src/components/JumboTron.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+<template>
+  <div class="container">
+    <img src="../assets/vue.svg" alt="" />
+  </div>
+</template>
+<style>
+img {
+  width: 200px;
+}
+img:hover {
+  scale: 1.3;
+}
+</style>
