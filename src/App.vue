2cf23a12fd78b89322c63345e9ddffa7e8aba583
Ciaowilly3 <muscomassi@gmail.com>
2022-12-01 15:11:33 +0100

    Scritta comparsa

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script>
+import Titolo from "./components/AppTitle.vue";
+export default {
+  components: { Titolo },
+};
+</script>
+
+<template><Titolo></Titolo></template>
+
+<style scoped></style>
